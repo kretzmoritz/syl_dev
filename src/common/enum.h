2979129e16a0475bd8e4be61f3144cb81ab9abce
Moritz Kretz <kretz.moritz@gmail.com>
2016-04-21 03:27:43 +0200

    Started building input context interface. Wrote helper macro to create enum entries with corresponding strings.

---
+++ src/common/enum.h
@@ -0,0 +1,75 @@
+///////////////////////////////////////
+//  (             (                
+//  )\ )     (    )\ )             
+// (()/((    )\  (()/(    (   )    
+//  /(_))\ )((_)  /(_))  ))\ /((   
+// (_))(()/( _   (_))_  /((_|_))\  
+// / __|)(_)) |   |   \(_)) _)((_) 
+// \__ \ || | |   | |) / -_)\ V /  
+// |___/\_, |_|   |___/\___| \_/   
+//      |__/                       
+//
+// 2016 Moritz Kretz
+///////////////////////////////////////
+
+#ifndef SYLDEV_COMMON_ENUM
+#define SYLDEV_COMMON_ENUM
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+#define ENUM_WITH_STRING(name, ...) \
+	struct name \
+	{ \
+		enum Type { __VA_ARGS__, Count }; \
+		class Converter \
+		{ \
+		public: \
+			Converter() \
+			{ \
+				std::string enumName = #name; \
+				std::string params = #__VA_ARGS__; \
+				size_t length = params.length(); \
+				std::ostringstream stream; \
+				for (size_t i = 0; i < length; ++i) \
+				{ \
+					if (isspace(params[i])) \
+					{ \
+						continue; \
+					} \
+					else if (params[i] == ',') \
+					{ \
+						std::string string = stream.str(); \
+						if (!string.empty()) \
+						{ \
+							m_strings.push_back(enumName + "::" + string); \
+							stream.str(""); \
+							stream.clear(); \
+						} \
+					} \
+					else \
+					{ \
+						stream << params[i]; \
+					} \
+				} \
+				std::string string = stream.str(); \
+				if (!string.empty()) \
+				{ \
+					m_strings.push_back(enumName + "::" + string); \
+				} \
+			} \
+			std::string ToString(Type _value) \
+			{ \
+				if (_value < m_strings.size()) \
+				{ \
+					return m_strings[_value]; \
+				} \
+				return ""; \
+			} \
+		private: \
+			std::vector<std::string> m_strings; \
+		}; \
+	}; \
+
+#endif
--- src/framework/input_context.h
+++ src/framework/input_context.h
@@ -15,13 +15,22 @@
 #ifndef SYLDEV_FRAMEWORK_INPUTCONTEXT
 #define SYLDEV_FRAMEWORK_INPUTCONTEXT
 
+#include <string>
+
 #include "../common/namespace.h"
+#include "raw_input_constants.h"
 
 BEGIN_2_NAMESPACES(SylDev, Framework)
 
 class InputContext
 {
+public:
+	void ReadFromFile(std::string _file);
+	void WriteToFile(std::string _file);
 
+	unsigned int MapButtonToAction(RawInputButton::Type _button, unsigned int _action);
+	unsigned int MapButtonToState(RawInputButton::Type _button, unsigned int _state);
+	unsigned int MapAxisToRange(RawInputAxis::Type _axis, unsigned int _range);
 };
 
 END_2_NAMESPACES
